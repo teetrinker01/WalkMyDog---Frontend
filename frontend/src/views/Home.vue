c985846c7c6ce36fb3a0c0fb14666f6359358f63
bentagai <bentagai@protonmail.com>
2020-04-08 14:35:56 +0100

    vue guay

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,33 +1,25 @@
 <template>
   <div>
     <v-app-bar dense flat>
-      <v-toolbar-title>
-        My Dog Walker</v-toolbar-title>
+      <v-toolbar-title> My Dog Walker</v-toolbar-title>
     </v-app-bar>
     <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-        <h1>Share the love dogs</h1>
+      :src="require('../assets/logo.svg')"
+      class="my-3"
+      contain
+      height="200"
+    />
+    <h1>Share the love dogs</h1>
+
     <v-btn color="green" dark v-on="on">
       <a href="./views.SignUp.vue">Sign Up</a>
     </v-btn>
     <v-btn color="green" dark v-on="on">
       LogIn
     </v-btn>
-
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
-
-<style lang="css" scoped>
-
-
-</style>
