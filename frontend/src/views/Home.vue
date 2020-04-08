53af4ebd61457667055c0a10efc6ff445e8ff36b
bentagai <bentagai@protonmail.com>
2020-04-08 13:41:34 +0100

    vue ok

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-app-bar dense flat>
+      <v-toolbar-title>
+        My Dog Walker</v-toolbar-title>
+    </v-app-bar>
+    <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+        <h1>Share the love dogs</h1>
+    <v-btn color="green" dark v-on="on">
+      <a href="./views.SignUp.vue">Sign Up</a>
+    </v-btn>
+    <v-btn color="green" dark v-on="on">
+      LogIn
+    </v-btn>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  export default {
+    
   }
-};
 </script>
+
+<style lang="css" scoped>
+
+
+</style>
