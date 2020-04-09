0755c137e301189f21347e00d058b865b61386cd
PedroConcejo <pc_89@hotmail.es>
2020-04-09 17:45:59 +0100

    dolist

--- src/views/Listdog.vue
+++ src/views/Listdog.vue
@@ -7,9 +7,31 @@
           <Verticalsearchbar />
         </v-col>
         <v-col>
-          <ul>
-            <Listcard v-for="dog in dogs" :key="dog.id" :dog="dog" />
-          </ul>
+          <v-card
+            v-for="dog in dogs"
+            :key="dog.id"
+            :dog="dog"
+            class="mx-auto"
+            max-width="250"
+          >
+            <v-flex justify-center>
+              <v-img
+                class="white--text align-end"
+                height="250px"
+                src="https://images.dog.ceo/breeds/terrier-bedlington/n02093647_3594.jpg"
+              >
+                <v-card-title>{{ dog.name }}</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pb-0">{{ dog.breed }}</v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn :style="{ left: '50%', transform: 'translateX(-50%)' }">
+                  <span>Walk Me</span>
+                </v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -19,7 +41,6 @@
 <script>
 import Verticalsearchbar from "../components/Verticalsearchbar.vue";
 import Navigationbar from "../components/Navigationbar.vue";
-import Listcard from "../components/Listcard.vue";
 import api from "../services/Api.js";
 
 export default {
@@ -31,8 +52,7 @@ export default {
   },
   components: {
     Verticalsearchbar,
-    Navigationbar,
-    Listcard
+    Navigationbar
   },
   methods: {
     async getAllDogs() {
